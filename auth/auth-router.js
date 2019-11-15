e57b495dd3a54fef8032c8aadbb519a377c1112a
unknown <neduvinci@gmail.com>
2019-11-15 16:04:23 +0100

    added test for login end point

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,34 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  const { username, password } = req.body
+  db.findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res
+          .status(200)
+          .json({
+          message: "Welcome " + user.username,
+          token: token
+        })
+      }
+      else {
+        res
+          .status(401)
+          .json({
+          message: "Invalid credentials"
+        })
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+        message: "There was a failure",
+        error: error
+      })
+    })
 });
 
 module.exports = router;
--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,6 +1,7 @@
 const server = require('../api/server')
 const request = require('supertest')
 const db = require('../database/dbConfig');
+const User = require('./auth-router')
 
 beforeEach(() => {
   return db('users').truncate();
@@ -27,5 +28,40 @@ describe('POST /register', () => {
         
         expect(res.type).toBe('application/json')
         expect(res.body).toEqual({id: 1, username: 'mike'})
+    })
+})
+
+describe('POST /login', () => {
+    it('returns status of 200', async () => {
+        await request(server)
+            .post('/api/auth/register')
+            .send({
+                username: 'mike',
+                password: '1234'
+            });
+        const res = await request(server)
+            .post('/api/auth/login')
+            .send({
+                username: 'mike',
+                password: '1234'
+            });           
+        expect(res.status).toBe(200)            
+    })
+
+    it('returns a token', async () => {
+        await request(server)
+            .post('/api/auth/register')
+            .send({
+                username: 'mike',
+                password: '1234'
+            });
+        const res = await request(server)
+            .post('/api/auth/login')
+            .send({
+                username: 'mike',
+                password: '1234'
+            });
+        
+        expect(res.body).toHaveProperty('token')
     })
 })
